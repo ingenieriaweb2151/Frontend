a4d12412fd6ff7907b3c2dd2ac54ce065dc7c964
León Andrés Avila Canale <leonavila9@gmail.com>
2015-04-29 19:50:19 -0600

    Solucion Banco de Proyectos

--- data/funciones.php
+++ data/funciones.php
@@ -37,28 +37,29 @@ function conectaBD()
 	//Servidor, Usuario, Contraseña
 	$conexion =  mysql_connect("localhost","root","");
 	//Seleccionamos la BD
-	mysql_select_db("pw15");
+	mysql_select_db("residencias");
 	return $conexion;
 }	
 
 
 function ValidaEntrada()
 {
-	$u = GetSQLValueString($_POST["usuario"],"text");
-	$c = GetSQLValueString(md5($_POST["clave"]),"text");
+	$u = GetSQLValueString($_POST["aluctr"],"text");
+	$c = GetSQLValueString(md5($_POST["alupas"]),"text");
+	$tipousuario = GetSQLValueString($_POST["tu"],"text");
 	//Conectar a la BD
-	$conexion = conectaBD();
+	$conexion = conectaBD($tipousuario);
 	//Preparar la consulta SQL
-	$consulta  = sprintf("select * from usuarios where usuario=%s and clave=%s",$u,$c);
+	$consulta  = sprintf("select alunom from dalumn where aluctr=%s and alupas=%s",$u,$c);
 	//Ejecutamos la consulta.
 	$resultado = mysql_query($consulta);
 	//Validamos los datos.
 	$res = false; //Saber el correcto
-	$nombre = ""; //Nombre completo
+	$nomalum = ""; //Nombre completo
 	if($registro = mysql_fetch_array($resultado))
 	{
 		$res = true;
-		$nombre = $registro["nombre"]." ".$registro["apellido"];
+		$nombre = $registro["nomalum"]." ".$registro["aluapp"];
 	}
 	$salidaJSON = array('respuesta' => $res,
 						'nombre'    => $nombre);
@@ -66,46 +67,46 @@ function ValidaEntrada()
 	print json_encode($salidaJSON);
 }
 
-function consultaUsuario($usuario)
-{
-	$respuesta = false;
-	$conexion = conectaBD();
-	$consulta = sprintf("select usuario from usuarios where usuario=%s",$usuario);
-	$resconsulta = mysql_query($consulta); //ejecutamos la consulta.
-	if($registro = mysql_fetch_array($resconsulta))
-	{
-		$respuesta = true;
-	}
-	return $respuesta;
-}
+// function consultaUsuario($usuario)
+// {
+// 	$respuesta = false;
+// 	$conexion = conectaBD();
+// 	$consulta = sprintf("select usuario from usuarios where usuario=%s",$usuario);
+// 	$resconsulta = mysql_query($consulta); //ejecutamos la consulta.
+// 	if($registro = mysql_fetch_array($resconsulta))
+// 	{
+// 		$respuesta = true;
+// 	}
+// 	return $respuesta;
+// }
 
-function GuardaUsuario()
+function GuardaProyecto()
 {
-	$usuario     = GetSQLValueString($_POST["usuario"],"text");
-	$nombre      = GetSQLValueString($_POST["nombre"],"text");
-	$apellido    = GetSQLValueString($_POST["apellido"],"text");
-	$tipousuario = GetSQLValueString($_POST["tipousuario"],"long");
-	$estatus     = GetSQLValueString($_POST["estatus"],"text");
-	$clave       = GetSQLValueString(md5($_POST["clave"]),"text");
-	$repiteclave = GetSQLValueString(md5($_POST["repiteclave"]),"text");
+	$nombre_empresa     = GetSQLValueString($_POST["usuario"],"text");
+	$direccion      = GetSQLValueString($_POST["direccion"],"text");
+	$telefono    = GetSQLValueString($_POST["telefono"],"text");
+	$encargado = GetSQLValueString($_POST["encargado"],"long");
+	$nombre_proyecto     = GetSQLValueString($_POST["nombre_proyecto"],"text");
+	$carrera       = GetSQLValueString($_POST["carrera"],"text");
+	$cupos = GetSQLValueString($_POST["cupos"],"text");
 	$respuesta   = false; 
 	if($clave == $repiteclave)
 	{
 		$conexion    = conectaBD();
 		if(consultaUsuario($usuario) == false)
 		{
-			$consulta = sprintf("insert into usuarios values(%s,%s,%s,%s,%d,%s)",$usuario,$clave,$nombre,$apellido,$tipousuario,$estatus);
+			$consulta = sprintf("insert into proyectos values(%s,%s,%s,%s,%d,%s)",$nombre_empresa,$direccion,$telefono,$encargado,$nombre_proyecto,$carrera,$cupos);
 			$resconsulta = mysql_query($consulta);
 			if(mysql_affected_rows() > 0)
 				$respuesta = true;
 		}
-		else
-		{
-			$consulta = sprintf("update usuarios set clave=%s,nombre=%s,apellido=%s,tipousuario=%d,estatus=%s where usuario=%s",$clave,$nombre,$apellido,$tipousuario,$estatus,$usuario);
-			$resconsulta = mysql_query($consulta);
-			if(mysql_affected_rows() > 0)
-				$respuesta = true;
-		}
+		// else
+		// {
+		// 	$consulta = sprintf("update proyectos set clave=%s,nombre=%s,apellido=%s,tipousuario=%d,estatus=%s where usuario=%s",$clave,$nombre,$apellido,$tipousuario,$estatus,$usuario);
+		// 	$resconsulta = mysql_query($consulta);
+		// 	if(mysql_affected_rows() > 0)
+		// 		$respuesta = true;
+		// }
 	}
 	$salidaJSON = array('respuesta' => $respuesta);
 	print json_encode($salidaJSON);
@@ -152,13 +153,176 @@ function EliminaUsuario()
 	print json_encode($salidaJSON);
 }
 
+function ValidaAluProy(){
+	$u = GetSQLValueString($_POST["aluctr"],"text");
+	$c = GetSQLValueString(md5($_POST["alupas"]),"text");
+	//Conectar a la BD
+	$conexion = conectaBD();
+	//Preparar la consulta SQL
+	$consulta  = sprintf("select P.nombre as nombreproy,P.numresi,P.objetiv,P.justifi,P.nomresp,E.nombre as nombreemp,E.telef,DA.alunom,DA.aluapp from dalumn DA left join asignproyectos AP ON AP.aluctr=DA.aluctr left join proyectos P ON AP.cveproy=P.cveproy left join empresas E ON AP.cveempr=E.cveempr where DA.aluctr=%s and DA.alupas=%s",$u,$c);
+	//Ejecutamos la consulta.
+	$resultado = mysql_query($consulta);
+	//Validamos los datos.
+	$res = false; //Saber el correcto
+	$renglones = "";
+	$nombre = ""; //Nombre completo
+	$pnom = "";
+	$pnumr = 0;
+	$pobj = "";
+	$pjust = "";
+	$pnomresp = "";
+	$enom = "";
+	$etel = "";
+	if($registro = mysql_fetch_array($resultado))
+	{
+		$res = true;
+		$nombre = $registro["alunom"]." ".$registro["aluapp"];
+		$pnom = $registro["nombreproy"];
+		$pnumr = $registro["numresi"];
+		$pobj = $registro["objetiv"];
+		$pjust = $registro["justifi"];
+		$pnomresp = $registro["nomresp"];
+		$enom = $registro["nombreemp"];
+		$etel = $registro["telef"];
+	}
+	$consulta2  = sprintf("select P.nombre as nombreproy,P.numresi,P.objetiv,P.justifi,P.nomresp,E.nombre as nombreemp,E.telef from proyectos P inner join empresas E ON P.cveempr=E.cveempr"); 
+	$resultado2 = mysql_query($consulta2);
+
+	if($pnom!=NULL){
+		$renglones.="<tr>";
+		$renglones.="<th>Nombre Proyecto</th>";
+		$renglones.="<th>Objetivo</th>";
+		$renglones.="<th>Justificacion</th>";
+		$renglones.="<th>Empresa</th>";
+		$renglones.="<th>Encargado</th>";
+		$renglones.="<th>Telefono</th>";
+		$renglones.="<th>Cupos</th>";
+		$renglones.="</tr>";
+		while($registro = mysql_fetch_array($resultado2)){
+			$renglones.="<tr>";
+			$renglones.="<td>".$registro["nombreproy"]."</td>";
+			$renglones.="<td>".$registro["objetiv"]."</td>";
+			$renglones.="<td>".$registro["justifi"]."</td>";
+			$renglones.="<td>".$registro["nombreemp"]."</td>";
+			$renglones.="<td>".$registro["nomresp"]."</td>";
+			$renglones.="<td>".$registro["telef"]."</td>";
+			$renglones.="<td>".$registro["numresi"]."</td>";
+			$renglones.="</tr>";
+
+		}
+	}
+	else{
+		$renglones.="<tr>";
+		$renglones.="<th>Nombre Proyecto</th>";
+		$renglones.="<th>Objetivo</th>";
+		$renglones.="<th>Justificacion</th>";
+		$renglones.="<th>Empresa</th>";
+		$renglones.="<th>Encargado</th>";
+		$renglones.="<th>Telefono</th>";
+		$renglones.="<th>Cupos</th>";
+		$renglones.="<th>Cargar</th>";
+		$renglones.="</tr>";
+		while($registro = mysql_fetch_array($resultado2)){
+			$renglones.="<tr>";
+			$renglones.="<td>".$registro["nombreproy"]."</td>";
+			$renglones.="<td>".$registro["objetiv"]."</td>";
+			$renglones.="<td>".$registro["justifi"]."</td>";
+			$renglones.="<td>".$registro["nombreemp"]."</td>";
+			$renglones.="<td>".$registro["nomresp"]."</td>";
+			$renglones.="<td>".$registro["telef"]."</td>";
+			$renglones.="<td>".$registro["numresi"]."</td>";
+			$renglones.="<td>";
+			$renglones.="<input type='checkbox' name=".$registro["nombreproy"].">";
+			$renglones.="</td>";
+			$renglones.="</tr>";
+		}
+	}
+	$salidaJSON = array('respuesta' => $res,
+						'nombre'    => $nombre,
+						'pnom'		=> $pnom,
+						'pnumr'		=> $pnumr,
+						'pobj'		=> $pobj,
+						'pjust'		=> $pjust,
+						'pnomresp'	=> $pnomresp,
+						'enom'		=> $enom,
+						'etel'		=> $etel,
+						'renglones' => $renglones); 
+	//Codificamos a JSON el array asociativo.
+	print json_encode($salidaJSON);
+}
+
+function LlenarTablaProy(){
+	$conexion = conectaBD();
+	//Preparar la consulta SQL
+	$consulta  = sprintf("select P.nombre as nombreproy,P.numresi,P.objetiv,P.justifi,P.nomresp,E.nombre as nombreemp,E.telef from proyectos P inner join empresas E ON P.cveempr=E.cveempr");
+	//Ejecutamos la consulta.
+	$resultado = mysql_query($consulta);
+	//Validamos los datos.
+	$res = false; //Saber el correcto
+	$renglones = "";
+	if($pnom!=NULL){
+		$renglones.="<tr>";
+		$renglones.="<th>Nombre Proyecto</th>";
+		$renglones.="<th>Objetivo</th>";
+		$renglones.="<th>Justificacion</th>";
+		$renglones.="<th>Empresa</th>";
+		$renglones.="<th>Encargado</th>";
+		$renglones.="<th>Telefono</th>";
+		$renglones.="<th>Cupos</th>";
+		$renglones.="</tr>";
+		while($registro = mysql_fetch_array($resultado)){
+			$res = true;
+			$renglones.="<tr>";
+			$renglones.="<td>".$registro["nombreproy"]."</td>";
+			$renglones.="<td>".$registro["objetiv"]."</td>";
+			$renglones.="<td>".$registro["justifi"]."</td>";
+			$renglones.="<td>".$registro["nombreemp"]."</td>";
+			$renglones.="<td>".$registro["nomresp"]."</td>";
+			$renglones.="<td>".$registro["telef"]."</td>";
+			$renglones.="<td>".$registro["numresi"]."</td>";
+			$renglones.="</tr>";
+
+		}
+	}
+	else{
+		$renglones.="<tr>";
+		$renglones.="<th>Nombre Proyecto</th>";
+		$renglones.="<th>Objetivo</th>";
+		$renglones.="<th>Justificacion</th>";
+		$renglones.="<th>Empresa</th>";
+		$renglones.="<th>Encargado</th>";
+		$renglones.="<th>Telefono</th>";
+		$renglones.="<th>Cupos</th>";
+		$renglones.="<th>Cargar</th>";
+		$renglones.="</tr>";
+		while($registro = mysql_fetch_array($resultado)){
+			$res = true;
+			$renglones.="<tr>";
+			$renglones.="<td>".$registro["nombreproy"]."</td>";
+			$renglones.="<td>".$registro["objetiv"]."</td>";
+			$renglones.="<td>".$registro["justifi"]."</td>";
+			$renglones.="<td>".$registro["nombreemp"]."</td>";
+			$renglones.="<td>".$registro["nomresp"]."</td>";
+			$renglones.="<td>".$registro["telef"]."</td>";
+			$renglones.="<td>".$registro["numresi"]."</td>";
+			$renglones.="<td>";
+			$renglones.="<input type='checkbox' name=".$registro["nombreproy"].">";
+			$renglones.="</td>";
+			$renglones.="</tr>";
+		}
+	}
+	$salidaJSON = array('respuesta'	=> $res,
+						'renglones'	=> $renglones);
+	print json_encode($salidaJSON);
+}
+
 //Opciones a ejecutar.
 $opcion = $_POST["opc"];
 switch ($opcion) {
 	case 'validaentrada':
 		ValidaEntrada();
 		break;
-	case 'guardausuario':
+	case 'guardaproyecto':
 		GuardaUsuario();
 		break;
 	case 'mostrarDatosUsuario':
@@ -167,6 +331,11 @@ switch ($opcion) {
 	case 'EliminaUsuario':
 		EliminaUsuario();
 		break;
+	case 'validaAluProy':
+		ValidaAluProy();
+		break;
+	case 'llenarTablaProy':
+		LlenarTablaProy();
 	default:
 		# code...
 		break;
--- js/eventos.js
+++ js/eventos.js
@@ -1,11 +1,9 @@
-var inicio = function()
-{
-	//Declaramos las funciones.
-	var validaUsuario = function()
-	{
+var inicio = function(){
+
+	var validaUsuario = function(){
 		var u = $("#txtUsuario").val();
 		var c = $("#txtClave").val();
-		var parametros = "opc=validaentrada"+"&usuario="+u+"&clave="+c+"&id="+Math.random();
+		var parametros = "opc=validaAluProy"+"&aluctr="+u+"&alupas="+c+"&id="+Math.random();
 		if(u!="" && c!="")
 		{
 			$.ajax({
@@ -19,12 +17,21 @@ var inicio = function()
 					{
 						$("#panelEntrada").hide("slow");
 						$("#nav1").show("slow");
-
 						$("#btnEntregas").show("slow");
 						$("#informacion").show("slow");
 						$("#btnIngresar").hide("slow");
-						$("#banco").hide();
+
+
+						$("#tablaproy").html(response.renglones);
+						$("#tablaproy").show();
+
 						alert("Bienvenido: "+response.nombre);
+						
+						anoalumno();
+
+						$("#bienvenido").show("slow");
+						document.getElementById("ttipo").innerHTML = response.nombre;
+						document.getElementById("usuario").innerHTML = response.nombre;
 					}
 					else
 						alert("Nombre de usuario y/o contraseña incorrectos");
@@ -39,58 +46,105 @@ var inicio = function()
 		// 	$("nav").show("slow");
 		// }
 	}
+		
+	var llenarTablaProy = function(cargado){
 
-	var traeInicio = function()
-	{
-		// $.ajax({
-		//   url: 'http://api.randomuser.me/',
-		//   dataType: 'json',
-		//   success: function(data){
-		//   	$("img").attr("src",data.results[0].user.picture.medium);
-		//   	$("h1").html(data.results[0].user.name.first+' '+
-		//   		         data.results[0].user.name.last);
-		//   	$("h2").html(data.results[0].user.email);
-		//   }
-		// });
+		//$u = GetSQLValueString($_POST["aluctr"],"text");
+		//$c = GetSQLValueString(md5($_POST["alupas"]),"text");
+
+		var c = cargado;
+		var parametros = "opc=llenarTablaProy"+"&cargado="+c+"&id="+Math.random();
+		$.ajax({
+				cache:false,
+				url: "data/funciones.php",
+				type: "POST",
+				dataType: "json",
+				data: parametros,
+				success: function(response){
+					if(response.respuesta == true) 
+					{
+						alert("Tabla proyecto");
+						$("#tablaproy").html(response.renglones);
+						$("#tablaproy").show();
+					}
+					else
+						alert("No hay proyectos");
+
+						$("#tablaproy").html(response.renglones);
+				}
+			});
+	}
+
+	var validaAluProy = function(response){
+			
+		// if (nomproy != null){
+		// 	document.getElementById("tusuario").innerHTML = "Maestro: "
+		// 	$("#btnRegistrar").show("slow");s
+		// 	$("#proyectosbox").show("slow");
+
+		// 	//Cambio de nombre de botones
+		// 	document.getElementById("btnEntregas").innerHTML = "Revisiones"
+		// }
+		// else{
+		// 	document.getElementById("tusuario").innerHTML = "Alumno: "
+		// }
+	}
+
+	var anoalumno = function (){
+		var u = $("#txtUsuario").val();
+		var ano = parseInt(u.substring(0,2));
+			if(ano>=9){
+				$("#PlanViejo").hide();
+				$("#PlanNuevo").show();
+			}
+			else{
+				$("#PlanNuevo").hide();
+				$("#PlanViejo").show();
+			}
+	}
+
+	var traeInicio = function(){
 
 		$("#informacion").show();
 		$("#documentacion").hide();
 		$("#banco").hide();
 		$("#entregas").hide();
-
+		$("#panelEntrada").hide("slow");
+		$("#altaProyectos").hide("slow");
+		$("#entregas").hide("slow");
 	}
 
-	var traeBanco = function ()
-	{
+	var traeBanco = function (){
 		$("#informacion").hide();
 		$("#documentacion").hide();
 		$("#entregas").hide();
 		$("#banco").show();
-
+		$("#panelEntrada").hide("slow");
+		$("#altaProyectos").hide("slow");
+		$("#entregas").hide("slow");
 	}
 
-
-		var traeDocumentacion = function()
-	{
+	var traeDocumentacion = function(){
 		$("#informacion").hide();
 		$("#documentacion").show();
 		$("#banco").hide();
 		$("#entregas").hide();
-
+		$("#panelEntrada").hide("slow");
+		$("#altaProyectos").hide("slow");
+		$("#entregas").hide("slow");
 	}
 
-	  var traeEntregas = function ()
-	  {
-	  	$("#entregas").show();
+	var traeEntregas = function(){
+	 	$("#entregas").show();
 		$("#informacion").hide();
 		$("#documentacion").hide();
 		$("#banco").hide();
+		$("#panelEntrada").hide("slow");
+		$("#altaProyectos").hide("slow");
+	 }
 
-	  }
 
-
-	var teclaUsuario = function(tecla)
-	{
+	var teclaUsuario = function(tecla){
 		if(tecla.which == 13) //Enter
 		{
 			//Pongo el cursor en el cuadro 
@@ -99,42 +153,41 @@ var inicio = function()
 		}
 	}
 
-	var teclaClave = function(tecla)
-	{
+	var teclaClave = function(tecla){
 		if(tecla.which == 13)
 		{
 			validaUsuario();
 		}
 	}
 
-	var DivUsuarios = function()
-	{
-		$("#altaUsuarios").show("slow");
-		$("#btnGuardaUsuario").show();
-		$("#btnEliminaUsuario").hide();
+	var DivUsuarios = function(){
+		$("#altaProyectos").show("slow");
+		$("#btnGuardaProyecto").show();
+		$("#btnEliminaProyecto").hide();
 	}
 
-		var Ingresar = function()
-	{
+	var Ingresar = function(){
 		$("#panelEntrada").show("slow");
-		$("#nav1").hide("slow");
+		// $("#nav1").hide("slow");
 		$("#informacion").hide("slow");
 		$("#entregas").hide();
-
+		$("#banco").hide();
+		$("#seccionlinks").show();
+		$("#documentacion").hide();
+		$("#docsAmbos").hide("slow");
+		$("#docsGenerales").show("slow");
 	}
 
-	var GuardaUsuario = function()
-	{
-		var u = $("#txtNombreUsuario").val();
-		var n = $("#txtNombre").val();
-		var a = $("#txtApellido").val();
-		var t = $("#txtTipoUsuario").val();
-		var e = $("#txtEstatus").val();
-		var c = $("#txtClaveUsuario").val();
-		var r = $("#txtRepiteClave").val();
-		if(c == r)
-		{
-			var parametros = "opc=guardausuario"+"&usuario="+u+"&nombre="+n+"&apellido="+a+"&tipousuario="+t+"&estatus="+e+"&clave="+c+"&repiteclave="+r+"&id="+Math.random();
+	var GuardaProyecto = function(){
+		var u = $("#txtNombreEmpresa").val();
+		var n = $("#txtDireccion").val();
+		var a = $("#txtTelefono").val();
+		var t = $("#txtEncargado").val();
+		var e = $("#txtNombreProyecto").val();
+		var c = $("#txtCarrrera").val();
+		var r = $("#txtCupos").val();
+		
+			var parametros = "opc=guardaproyecto"+"&nombre_empresa="+u+"&direccion="+n+"&telefono="+a+"&encargado="+t+"&nombre_proyecto="+e+"&carrera="+c+"&cupos="+r+"&id="+Math.random();
 			$.ajax({
 				cache:false,
 				type: "POST",
@@ -144,22 +197,19 @@ var inicio = function()
 				success: function(response){
 					if(response.respuesta == true)
 					{
-						alert("Usuario actualizado con éxito");
+						alert("Proyecto registrado con éxito");
 					}
 					else
-						alert("No se ha podido actualizar al usuario");
+						alert("No se ha podido registrar el proyecto");
 				},
 				error: function(xhr,ajaxOption,x){
 					alert("Sin conexión");
 				}
 			});
-		}
-		else
-			alert("Las claves no coinciden");
+		$("#altaProyectos").hide("slow");
 	}
 
-	var mostrarDatosUsuario = function()
-	{
+	var mostrarDatosUsuario = function(){
 		var u = $("#txtNombreUsuario").val();
 		var parametros = "opc=mostrarDatosUsuario"+
 						 "&usuario="+u+
@@ -187,17 +237,14 @@ var inicio = function()
 		});
 	}
 
-	var teclaNombreUsuario = function(tecla)
-	{
+	var teclaNombreUsuario = function(tecla){
 		if(tecla.which == 13) //Enter
 		{
 			mostrarDatosUsuario();
 		}		
 	}
 
-
-	var EliminaUsuario = function()
-	{
+	var EliminaUsuario = function(){
 		var u = $("#txtNombreUsuario").val();
 		var parametros = "opc=EliminaUsuario"+"&usuario="+u+"&id="+Math.random();
 		$.ajax({
@@ -227,23 +274,15 @@ var inicio = function()
 	$("#txtClave").on("keypress",teclaClave);
 	$("#btnDivUsuarios").on("click",DivUsuarios);
 	$("#btnDivBanco").on("click",traeBanco);
-	$("#btnGuardaUsuario").on("click",GuardaUsuario);
+	$("#btnGuardaProyecto").on("click",GuardaProyecto);
 	$("#txtNombreUsuario").on("keypress",teclaNombreUsuario);
 	$("#btnDivDocumentacion").on("click",traeDocumentacion);
 	$("#btnEliminaUsuario").on("click",EliminaUsuario);
-
-
-	// Nuevas Acciones
-
 	$("#btnIngresar").on("click",Ingresar);
-
-
-
+	$("#btnRegistrar").on("click",DivUsuarios);
 }
+
 $(document).on("ready",inicio);
-
-
-
 
 
 
